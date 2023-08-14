8bc16d07acb167e2d527384eca112a685008b035
turandott <greyvilka@gmail.com>
2023-08-14 14:57:24 +0300

    fix: time offset

--- src/helpers/timeCheck.ts
+++ src/helpers/timeCheck.ts
@@ -4,11 +4,22 @@ function isValidTime(time: string): boolean {
 }
 
 function timeParser(time: string): number[] {
-  const [hours, minutes] = time.split(':').map(Number);
+  let [hours, minutes] = time.split(':').map(Number);
 
   if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
     throw new Error('Invalid time');
   }
+
+  //for timezone offset
+  let timezoneOffset = 3;
+  let adjustedHours = hours - timezoneOffset;
+  if (adjustedHours < 0) {
+    adjustedHours += 24;
+  } else if (adjustedHours > 23) {
+    adjustedHours -= 24;
+  }
+
+  hours = adjustedHours;
 
   return [hours, minutes];
 }
